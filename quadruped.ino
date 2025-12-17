3aa14d64003a20c51f30cdf12a3def1580df3e38
alamtaab <167731823+alamtaab@users.noreply.github.com>
2025-12-16 23:12:24 -0800

    Update quadruped.ino

--- quadruped.ino
+++ quadruped.ino
@@ -1,9 +1,40 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN 110 // min pulse width microsec
+#define SERVOMAX 510   // max pulse width microsec
+
+#define FREQ 50 // servo frequency in Hz
+
+uint8_t channel = 0; // driver channel
+int degrees = 90;
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  pwm.begin();
+  pwm.setOscillatorFrequency(25000000); // 25MHz internal clock
+  pwm.setPWMFreq(FREQ);
+  delay(10);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+
+  if (Serial.available() > 0) {
+    int read = Serial.parseInt();
+    degrees = read;
+    // if (read != 0) degrees = read;
+  }
+
+  uint16_t pulselen = map(degrees, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(channel, 0, pulselen);
+  delay(10);
+  
 }
