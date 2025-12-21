d8609a7f48585dbe8b0e2ea170e8e7642ef9c659
alamtaab <167731823+alamtaab@users.noreply.github.com>
2025-12-20 23:01:21 -0800

    Update Leg.cpp

--- firmware/lib/Leg/Leg.cpp
+++ firmware/lib/Leg/Leg.cpp
@@ -1,6 +1,6 @@
 #include <Leg.h>
 
-Leg::Leg(LegConfig config) : _cfg(config) {
+Leg::Leg(Adafruit_PWMServoDriver* driver, LegConfig config) : _cfg(config), _hip(driver, config.hip), _knee(driver, config.knee) {
 }
 
 void Leg::moveTo(float x, float y) {
@@ -10,8 +10,16 @@ void Leg::moveTo(float x, float y) {
     float L2 = _cfg.tibiaLength;
 
     float r2 = x * x + y * y;  // distance to end effector from origin
-    float innerAngle = (r2 - L1 * L1 - L2 * L2) / (2 * L1 * L2);
 
+    float maxReach = L1 + L2;
+
+    float distance = sqrt(r2);
+    if (distance > maxReach) {
+        x *= maxReach / distance; // scale to avoid reaching for impossible points
+        y *= maxReach / distance;
+    }
+
+    float innerAngle = (r2 - L1 * L1 - L2 * L2) / (2 * L1 * L2);
     innerAngle = constrain(innerAngle, -1.0f, 1.0f);
 
     // knee angle (IK)
@@ -22,33 +30,28 @@ void Leg::moveTo(float x, float y) {
     // angle of femur ccw from +X axis
     float hipRadians = atan2(y, x) - atan2(L2 * sin(kneeRadians), L1 + L2 * cos(kneeRadians));
 
-    float kneeDegrees = kneeRadians * 180.0 / M_PI;
+    float kneeDegrees = kneeRadians * (180.0 / M_PI);
 
     // hip servo is mounted such that servo 90deg = leg pointing down
     // + rotation is CW
     // ik angle is sign inverted such that CCW -> CW
-
-    float hipDegrees = -hipRadians * 180.0f / M_PI;
+    float hipDegrees = -hipRadians * (180.0f / M_PI);
 
     kneeDegrees = constrain(kneeDegrees, 25, 160);
-    pwm.setPWM(_cfg.hipPin, 0, degreesToPulse(hipDegrees));
-    pwm.setPWM(_cfg.kneePin, 0, degreesToPulse(kneeDegrees));
 
-    Serial.print("Angle1: ");
-    Serial.print(hipDegrees);
-    Serial.print(" Angle2: ");
-    Serial.println(kneeDegrees);
-    Serial.print("X: ");
-    Serial.print(x);
-    Serial.print(" Y: ");
-    Serial.println(y);
+    _hip.setAngle(hipDegrees);
+    _knee.setAngle(kneeDegrees);
+
+    // Serial.print("Angle1: ");
+    // Serial.print(hipDegrees);
+    // Serial.print(" Angle2: ");
+    // Serial.println(kneeDegrees);
+    // Serial.print("X: ");
+    // Serial.print(x);
+    // Serial.print(" Y: ");
+    // Serial.println(y);
 }
 
 void Leg::home() {
-    moveTo(8.0, 11.5);
-}
-
-uint16_t Leg::degreesToPulse(float angle) {
-    angle = constrain(angle, _cfg.minAngle, _cfg.maxAngle);
-    return (uint16_t)map(angle, _cfg.minAngle, _cfg.maxAngle, _cfg.minPulse, _cfg.maxPulse);
+    moveTo(_cfg.femurLength, _cfg.tibiaLength);
 }
